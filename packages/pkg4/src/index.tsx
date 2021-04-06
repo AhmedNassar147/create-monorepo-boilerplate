ffad8ea5d59d49410847b6142f0e0d590373650e
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2021-04-07 00:25:16 +0200

    support ts esm builds

---
+++ packages/pkg4/src/index.tsx
@@ -0,0 +1,14 @@
+/*
+ *
+ * Package:  `@domain/pkg4`.
+ *
+ */
+const ReactComponent = ({ name }: { name: string }): JSX.Element => (
+  <div>
+    <div>@domain/pkg4</div>
+    <p>ahmed nassar</p>
+    {name}
+  </div>
+);
+
+export default ReactComponent;
