852c51fe9379c386463428969b86196e598f462e
ahmed.nassar <ahmed.nassar@daybreakhotels.com>
2021-04-26 19:41:55 +0200

    throwing errors when `.env` file does not exist

--- internals/environment/geEnvVariables.js
+++ internals/environment/geEnvVariables.js
@@ -5,14 +5,31 @@
  */
 const { readFileSync } = require("fs");
 const { join } = require("path");
+const chalk = require("chalk");
 const { ENVIRONMENT_FILE_NAMES } = require("./constants");
 const getBaseEnvVariableValues = require("./getBaseEnvVariableValues");
 const findRootYarnWorkSpaces = require("../workspaces/findRootYarnWorkSpaces");
 const checkPathExistsSync = require("../scripts/checkPathExistsSync");
 
 const checkIsFileExistAndGetFilePath = (envFileName) => {
-  const fullFullPath = join(findRootYarnWorkSpaces(), envFileName);
-  return checkPathExistsSync(fullFullPath);
+  const rootPath = findRootYarnWorkSpaces();
+
+  const fullFullPath = join(rootPath, envFileName);
+  const doesFileExist = checkPathExistsSync(fullFullPath);
+
+  if (!doesFileExist && envFileName === ENVIRONMENT_FILE_NAMES.NORMAL) {
+    console.log(
+      chalk.bold.red(
+        `\`(getEnvVariables)\`: couldn't find ${chalk.white(
+          envFileName,
+        )} file in ${chalk.white(rootPath)}`,
+      ),
+    );
+
+    process.exit(1);
+  }
+
+  return doesFileExist;
 };
 
 const getEnvVariables = ({ mode }) => {
